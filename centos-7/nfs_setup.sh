a8a12446e0ea76ceb0298a575b6efb0ba7929ec3
Ben Hall <ben@benhall.me.uk>
2016-10-10 09:46:15 +0100

    Set NFS file permissions and remove fsid as it caused mounting issues

--- centos-7/nfs_setup.sh
+++ centos-7/nfs_setup.sh
@@ -2,8 +2,9 @@
 mkdir -p /exports
 for mnt in "$@"; do
   mkdir -p $mnt
+  chown nfsnobody:nfsnobody $mnt
   chmod 777 $mnt
-  echo "$mnt *(rw,sync,no_subtree_check,no_root_squash,fsid=0)" >> /etc/exports
+  echo "$mnt *(rw,sync,no_root_squash)" >> /etc/exports
 done
 
 exportfs -a
